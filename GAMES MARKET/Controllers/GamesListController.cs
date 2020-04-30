bcd8a8c00f531f6a150bff841275ea5ac2780af4
Mari Carmen Zoroa Moreno <marizoro99m@hotmail.com>
2020-05-01 00:05:18 +0200

    .....

---
+++ GAMES MARKET/Controllers/GamesListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GAMES_MARKET.Controllers
+{
+    public class GamesListController : Controller
+    {
+        // GET: GamesList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
