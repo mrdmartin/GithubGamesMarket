3f2898b89c8228b1784dd0719b859b9590e512d2
David Martín <david.martin.romero@outlook.com>
2020-05-08 19:30:38 +0200

    BBDD, capturas, novedades con preciototal por fecha

--- GAMES MARKET/Models/BO/BOJuegos.cs
+++ GAMES MARKET/Models/BO/BOJuegos.cs
@@ -92,12 +92,41 @@ namespace GAMES_MARKET.Controllers.BO
         }
         public List<JuegosModel> getJuegosDestacados()
         {
+            List<JuegosModel> listaJuegos = new List<JuegosModel>();
             List<JuegosModel> listaOfertas = new List<JuegosModel>();
             using (var bd = new Games_MarketEntities())
             {
                 listaOfertas = (from juegos in bd.juegos
+                                join plataformas in bd.plataformas
+                                on juegos.id_plataforma equals plataformas.id_plataforma
+                                join descuentos in bd.descuentos
+                                on juegos.id_juego equals descuentos.id_juego
+                                where (DateTime.Now > descuentos.inicio) && (DateTime.Now < descuentos.fin)
+                                orderby juegos.fecha_lanzamiento descending
+
+                                select new JuegosModel
+                                {
+                                    id_juego = juegos.id_juego,
+                                    nombre = juegos.nombre,
+                                    id_plataforma = juegos.id_plataforma,
+                                    precio = juegos.precio,
+                                    img_ruta = juegos.img_ruta,
+                                    trailer_url = juegos.trailer_url,
+                                    distribuidora = juegos.distribuidora,
+                                    descripcion = juegos.descripcion,
+                                    fecha_lanzamiento = juegos.fecha_lanzamiento,
+                                    nombre_plataforma = plataformas.nombre,
+                                    img_rutaPlataforma = plataformas.img_ruta,
+                                    descuento = descuentos.descuento
+                                }).ToList();
+
+                listaJuegos = (from juegos in bd.juegos
                                join plataformas in bd.plataformas
                                on juegos.id_plataforma equals plataformas.id_plataforma
+                               join descuentos in bd.descuentos
+                               on juegos.id_juego equals descuentos.id_juego into todas
+                               from descuentos in todas.DefaultIfEmpty()
+                               where descuentos == null || ((DateTime.Now < descuentos.inicio) && (DateTime.Now > descuentos.fin))
                                orderby juegos.fecha_lanzamiento descending
 
                                select new JuegosModel
@@ -112,10 +141,15 @@ namespace GAMES_MARKET.Controllers.BO
                                    descripcion = juegos.descripcion,
                                    fecha_lanzamiento = juegos.fecha_lanzamiento,
                                    nombre_plataforma = plataformas.nombre,
-                                   img_rutaPlataforma = plataformas.img_ruta
+                                   img_rutaPlataforma = plataformas.img_ruta,
+                                   descuento = 0
+
                                }).ToList();
+
+                listaJuegos = (listaJuegos.Concat(listaOfertas)).OrderByDescending(i => i.fecha_lanzamiento).ToList();
+
             }
-            return listaOfertas;
+            return listaJuegos;
         }
         public List<JuegosModel> getFullList()
         {
@@ -149,9 +183,7 @@ namespace GAMES_MARKET.Controllers.BO
 
                 listaJuegos = (from juegos in bd.juegos
                                join plataformas in bd.plataformas
-                              on juegos.id_plataforma equals plataformas.id_plataforma
-
-
+                               on juegos.id_plataforma equals plataformas.id_plataforma
                                join descuentos in bd.descuentos
                                on juegos.id_juego equals descuentos.id_juego into todas
                                from descuentos in todas.DefaultIfEmpty()
